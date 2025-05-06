4cfa120142df26d456961ae3220eb87b735da32f
srushhtireddy <srushtikakaraddi@gmail.com>
2025-05-06 21:11:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle mobile menu
+const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
+
